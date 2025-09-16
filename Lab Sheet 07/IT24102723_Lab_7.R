7fa4b811c6c5c2f8759fc7486e3417dd07449fe3
IT24102723 <it24102723@my.sliit.lk>
2025-09-16 23:05:28 +0530

    Add Lab_07

---
+++ Lab Sheet 07/IT24102723_Lab_7.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\User\\Desktop\\IT24102723 Lab_07")
+
+##Exercise 
+##Q1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+##Q2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+##Q3 part 1
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+##Q3 part 2
+qnorm(0.95, mean=100, sd=15)
+
